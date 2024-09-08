6bb829cc2cc9f5350d66cb3795c83dad793abd13
FJRG2007 <fjrg2007@tpeoficial.com>
2024-09-09 01:44:21 +0200

    � presences config

--- src/integrations/worker.py
+++ src/integrations/worker.py
@@ -6,6 +6,7 @@ def get_function(module_name, function_name="main"):
 
 
 def update_all_presences(status, sound_name="", sound_path=""):
+    if not config.presences: return
     threads = []
     for presence in config.service_presences:
         thread = threading.Thread(target=lambda presence=presence:get_function(presence, f"{"update" if status else "clear"}_{presence}_presence")(*(sound_name, sound_path) if status else ()))
